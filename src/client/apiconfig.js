440df5a8a905cff79c9008b6b3d8f9de5b48f209
yao <iris9292@163.com>
2020-05-03 12:36:09 +0800

    refactor: change API

--- src/client/apiconfig.js
+++ src/client/apiconfig.js
@@ -1,6 +1,6 @@
 export default {
-  getCountryInfo: 'localhost:3000/api/country/info',
-  getGeoInfo: 'localhost:3000/api/geo/info',
-  getWeatherInfo: 'localhost:3000/api/weather/info',
-  getImageInfo: 'localhost:3000/api/image/info'
+  getCountryInfo: '/api/country/info',
+  getGeoInfo: '/api/geo/info',
+  getWeatherInfo: '/api/weather/info',
+  getImageInfo: '/api/image/info'
 }
